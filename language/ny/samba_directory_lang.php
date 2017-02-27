3a56db80907cf84cca36ef14d4e4f4bc74fcbff1
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 04:41:18 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ny/samba_directory_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['samba_directory_app_description'] = 'The app Samba Directory amapereka zipangizo zofunika owerenga, magulu, nkhani ndi zina Directory.';
+$lang['samba_directory_app_name'] = 'Samba Directory';
+$lang['samba_directory_beta_warning'] = 'The app Samba Directory ndi beta!';
+$lang['samba_directory_domain_invalid'] = 'Samba Domeni ndi opanda pake.';
+$lang['samba_directory_initialize_directory'] = 'Initialize Directory';
+$lang['samba_directory_password_must_be_7_or_more_characters'] = 'Achinsinsi ayenera kukhala otchulidwa 7 kapena zambiri.';
+$lang['samba_directory_samba_directory_driver_description'] = 'Woyendetsa Samba misonkhano Directory';
+$lang['samba_directory_samba_directory_driver'] = 'Samba Directory Yoyendetsa';
+$lang['samba_directory_username_only'] = 'lolowera Only';
+$lang['samba_directory_with_domain'] = 'ndi domeni';
