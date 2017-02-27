ff9094296f57200c888b18c44a5ce694a26ed37c
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 04:44:48 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ja/samba_directory_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['samba_directory_app_description'] = 'Sambaのディレクトリアプリは、ユーザー、グループ、アカウントおよびその他のディレクトリサービスのために必要なツールを提供しています。';
+$lang['samba_directory_app_name'] = 'Sambaのディレクトリ';
+$lang['samba_directory_beta_warning'] = 'Sambaのディレクトリアプリはベータ版です！';
+$lang['samba_directory_domain_invalid'] = 'Sambaドメインは無効です。';
+$lang['samba_directory_initialize_directory'] = 'ディレクトリを初期化します';
+$lang['samba_directory_password_must_be_7_or_more_characters'] = 'パスワードは7文字以上でなければなりません。';
+$lang['samba_directory_samba_directory_driver_description'] = 'ディレクトリ・サービスのためのSambaのドライバ';
+$lang['samba_directory_samba_directory_driver'] = 'サンバDirectoryドライバ';
+$lang['samba_directory_username_only'] = 'ユーザー名のみ';
+$lang['samba_directory_with_domain'] = 'ドメインと';
