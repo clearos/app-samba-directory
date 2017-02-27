72d97f38932fd2d35567eba856904011010bd648
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 04:46:16 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lv/samba_directory_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['samba_directory_app_description'] = 'Samba Directory app nodrošina nepieciešamos rīkus, lai lietotājiem, grupām, kontu un citu uzziņu pakalpojumus.';
+$lang['samba_directory_app_name'] = 'Samba Directory';
+$lang['samba_directory_beta_warning'] = 'Samba Directory app ir beta!';
+$lang['samba_directory_domain_invalid'] = 'Samba domēna ir nederīgs.';
+$lang['samba_directory_initialize_directory'] = 'inicializēt Directory';
+$lang['samba_directory_password_must_be_7_or_more_characters'] = 'Parole ir 7 vai vairāk zīmes.';
+$lang['samba_directory_samba_directory_driver_description'] = 'Samba draiveris uzziņu pakalpojumus';
+$lang['samba_directory_samba_directory_driver'] = 'Samba Directory Driver';
+$lang['samba_directory_username_only'] = 'Tikai lietotājvārds';
+$lang['samba_directory_with_domain'] = 'ar domēnu';
