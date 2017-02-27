17688e949e6995836e8af0b1a4d2da85c97ff46a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 04:42:16 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/et/samba_directory_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['samba_directory_app_description'] = 'Samba Kataloog app annab vajalikud vahendid kasutajate, rühmade, raamatupidamise ja teiste kataloog teenuseid.';
+$lang['samba_directory_app_name'] = 'Samba Kataloog';
+$lang['samba_directory_beta_warning'] = 'Samba Kataloog app on beeta!';
+$lang['samba_directory_domain_invalid'] = 'Samba domeeni on kehtetu.';
+$lang['samba_directory_initialize_directory'] = 'Vorminda Kataloog';
+$lang['samba_directory_password_must_be_7_or_more_characters'] = 'Salasõna peab olema 7 või enam tähemärki.';
+$lang['samba_directory_samba_directory_driver_description'] = 'Samba juht kataloogiteenusteks';
+$lang['samba_directory_samba_directory_driver'] = 'Samba Kataloog Driver';
+$lang['samba_directory_username_only'] = 'kasutajanimi Ainult';
+$lang['samba_directory_with_domain'] = 'Domain';
