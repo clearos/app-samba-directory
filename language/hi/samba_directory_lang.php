9f21ff91376639db6c4a530ba6860f130f790828
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 04:43:41 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hi/samba_directory_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['samba_directory_app_description'] = 'सांबा निर्देशिका अनुप्रयोग उपयोगकर्ताओं, समूहों, खातों और अन्य निर्देशिका सेवाओं के लिए आवश्यक उपकरण प्रदान करता है।';
+$lang['samba_directory_app_name'] = 'सांबा निर्देशिका';
+$lang['samba_directory_beta_warning'] = 'सांबा निर्देशिका एप्लिकेशन बीटा है!';
+$lang['samba_directory_domain_invalid'] = 'सांबा डोमेन अमान्य है।';
+$lang['samba_directory_initialize_directory'] = 'इनिशियलाइज़ निर्देशिका';
+$lang['samba_directory_password_must_be_7_or_more_characters'] = 'पासवर्ड 7 या अधिक अक्षरों का होना चाहिए।';
+$lang['samba_directory_samba_directory_driver_description'] = 'निर्देशिका सेवाओं के लिए सांबा ड्राइवर';
+$lang['samba_directory_samba_directory_driver'] = 'सांबा निर्देशिका ड्राइवर';
+$lang['samba_directory_username_only'] = 'यूजर का नाम केवल';
+$lang['samba_directory_with_domain'] = 'डोमेन के साथ';
