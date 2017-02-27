15f3f86b60d00cc5649c9c4443d81b18348b87ae
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 04:43:51 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hu/samba_directory_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['samba_directory_app_description'] = 'A Samba Directory alkalmazás biztosítja a szükséges eszközöket a felhasználók, csoportok, számlák és egyéb címtár szolgáltatásokat.';
+$lang['samba_directory_app_name'] = 'Samba Directory';
+$lang['samba_directory_beta_warning'] = 'A Samba Directory alkalmazás béta!';
+$lang['samba_directory_domain_invalid'] = 'Samba Domain érvénytelen.';
+$lang['samba_directory_initialize_directory'] = 'inicializálása Directory';
+$lang['samba_directory_password_must_be_7_or_more_characters'] = 'Jelszó nem lehet 7 vagy több karakter.';
+$lang['samba_directory_samba_directory_driver_description'] = 'A Samba driver címtárszolgáltatásokat';
+$lang['samba_directory_samba_directory_driver'] = 'Samba Directory illesztőprogram';
+$lang['samba_directory_username_only'] = 'Csak a felhasználónevét';
+$lang['samba_directory_with_domain'] = 'Domain';
