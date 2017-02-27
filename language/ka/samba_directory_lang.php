be90479c1fe99de5562f5bd21734513c8ea570c2
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 04:44:58 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ka/samba_directory_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['samba_directory_app_description'] = 'სამბა დირექტორია app უზრუნველყოფს საჭირო ინსტრუმენტები წევრებს, ჯგუფები, ანგარიშები და სხვა მომსახურება მომსახურება.';
+$lang['samba_directory_app_name'] = 'Samba დირექტორია';
+$lang['samba_directory_beta_warning'] = 'სამბა დირექტორია app არის ბეტა!';
+$lang['samba_directory_domain_invalid'] = 'Samba Domain არასწორია.';
+$lang['samba_directory_initialize_directory'] = 'ინიციალიზაცია დირექტორია';
+$lang['samba_directory_password_must_be_7_or_more_characters'] = 'პაროლი უნდა იყოს 7 ან მეტი სიმბოლო.';
+$lang['samba_directory_samba_directory_driver_description'] = 'სამბა მძღოლი დირექტორია მომსახურება';
+$lang['samba_directory_samba_directory_driver'] = 'Samba დირექტორია Driver';
+$lang['samba_directory_username_only'] = 'ნიკი მხოლოდ';
+$lang['samba_directory_with_domain'] = 'დომენის';
