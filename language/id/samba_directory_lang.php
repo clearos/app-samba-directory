deb3f3599603e7d111854e6f1487ed3459f085d1
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 04:44:00 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/id/samba_directory_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['samba_directory_app_description'] = 'Samba Direktori app menyediakan alat yang diperlukan untuk pengguna, kelompok, rekening dan layanan direktori lainnya.';
+$lang['samba_directory_app_name'] = 'Direktori Samba';
+$lang['samba_directory_beta_warning'] = 'Samba Directory app beta!';
+$lang['samba_directory_domain_invalid'] = 'Samba Domain tidak valid.';
+$lang['samba_directory_initialize_directory'] = 'menginisialisasi direktori';
+$lang['samba_directory_password_must_be_7_or_more_characters'] = 'Sandi harus 7 atau lebih karakter.';
+$lang['samba_directory_samba_directory_driver_description'] = 'Sopir Samba untuk layanan direktori';
+$lang['samba_directory_samba_directory_driver'] = 'Samba Driver Directory';
+$lang['samba_directory_username_only'] = 'nama pengguna Hanya';
+$lang['samba_directory_with_domain'] = 'dengan Domain';
