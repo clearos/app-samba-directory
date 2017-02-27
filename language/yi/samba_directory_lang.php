44a85a53f2b44d56df31f81eac2a643879a70a84
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 04:52:38 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/yi/samba_directory_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['samba_directory_app_description'] = 'די סאַמבאַ דירעקטארי אַפּ גיט די נייטיק מכשירים פֿאַר ניצערס, גרופּעס, אַקאַונץ און אנדערע דירעקטארי באַדינונגען.';
+$lang['samba_directory_app_name'] = 'סאַמבאַ דירעקטארי';
+$lang['samba_directory_beta_warning'] = 'די סאַמבאַ דירעקטארי אַפּ איז ביתא!';
+$lang['samba_directory_domain_invalid'] = 'סאַמבאַ דאָמאַין איז פאַרקריפּלט.';
+$lang['samba_directory_initialize_directory'] = 'יניטיאַליזע דירעקטארי';
+$lang['samba_directory_password_must_be_7_or_more_characters'] = 'שפּריכוואָרט מוזן זייַן 7 אָדער מער אותיות.';
+$lang['samba_directory_samba_directory_driver_description'] = 'די סאַמבאַ שאָפער פֿאַר Directory סערוויסעס';
+$lang['samba_directory_samba_directory_driver'] = 'סאַמבאַ דירעקטארי דרייווער';
+$lang['samba_directory_username_only'] = 'נאמען בלויז';
+$lang['samba_directory_with_domain'] = 'מיט דאָמאַין';
