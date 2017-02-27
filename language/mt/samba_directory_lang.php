9c7bd342a32c16cd6ea71f9a2e574653f6cc2877
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 04:47:32 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mt/samba_directory_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['samba_directory_app_description'] = 'Il-app Samba Directory jipprovdi l-għodod meħtieġa għall-utenti, gruppi, kontijiet u servizzi tad-direttorju oħrajn.';
+$lang['samba_directory_app_name'] = 'Samba Direttorju';
+$lang['samba_directory_beta_warning'] = 'Il Samba Directory app huwa beta!';
+$lang['samba_directory_domain_invalid'] = 'Samba Dominju huwa invalidu.';
+$lang['samba_directory_initialize_directory'] = 'initialize Direttorju';
+$lang['samba_directory_password_must_be_7_or_more_characters'] = 'Password għandu jkun ta &#39;7 jew aktar karattri.';
+$lang['samba_directory_samba_directory_driver_description'] = 'Is-sewwieq Samba għas-servizzi tad-direttorju';
+$lang['samba_directory_samba_directory_driver'] = 'Samba Direttorju Sewwieq';
+$lang['samba_directory_username_only'] = 'Username biss';
+$lang['samba_directory_with_domain'] = 'bil Dominju';
