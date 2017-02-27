b08ad10e78cab5f43ac9cc10316f709fc9439252
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 04:41:27 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/zh/samba_directory_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['samba_directory_app_description'] = 'Samba的目录应用程序提供用户，组，帐户和其他目录服务的必要工具。';
+$lang['samba_directory_app_name'] = '桑巴目录';
+$lang['samba_directory_beta_warning'] = '桑巴目录应用程序是测试版！';
+$lang['samba_directory_domain_invalid'] = 'Samba域是无效的。';
+$lang['samba_directory_initialize_directory'] = '初始化目录';
+$lang['samba_directory_password_must_be_7_or_more_characters'] = '密码必须是7个或更多字符。';
+$lang['samba_directory_samba_directory_driver_description'] = 'Samba的驱动程序目录服务';
+$lang['samba_directory_samba_directory_driver'] = '桑巴Directory驱动程序';
+$lang['samba_directory_username_only'] = '只有用户名';
+$lang['samba_directory_with_domain'] = '随着域名';
