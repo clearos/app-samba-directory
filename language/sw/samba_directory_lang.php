4f8ce86ef1dad73bf13d45050cbf67e8feb18735
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 04:50:29 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sw/samba_directory_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['samba_directory_app_description'] = 'programu Samba Saraka hutoa zana muhimu kwa ajili ya watumiaji, vikundi, akaunti na huduma nyingine directory.';
+$lang['samba_directory_app_name'] = 'Samba Saraka';
+$lang['samba_directory_beta_warning'] = 'programu Samba Directory ni beta!';
+$lang['samba_directory_domain_invalid'] = 'Samba Domain ni batili.';
+$lang['samba_directory_initialize_directory'] = 'initialize Saraka';
+$lang['samba_directory_password_must_be_7_or_more_characters'] = 'Nywila lazima wahusika 7 au zaidi.';
+$lang['samba_directory_samba_directory_driver_description'] = 'dereva Samba kwa ajili ya huduma directory';
+$lang['samba_directory_samba_directory_driver'] = 'Samba Saraka Dereva';
+$lang['samba_directory_username_only'] = 'username tu';
+$lang['samba_directory_with_domain'] = 'pamoja Domain';
