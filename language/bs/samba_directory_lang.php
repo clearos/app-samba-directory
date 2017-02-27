faac644c6e7fdc7db983d45131d5426910a777fa
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 04:40:40 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/bs/samba_directory_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['samba_directory_app_description'] = 'Aplikacija Samba Directory daje potrebne alate za korisnike, grupe, račune i druge usluge direktorija.';
+$lang['samba_directory_app_name'] = 'Samba Directory';
+$lang['samba_directory_beta_warning'] = 'Samba Directory aplikacija je beta!';
+$lang['samba_directory_domain_invalid'] = 'Samba domena je nevažeća.';
+$lang['samba_directory_initialize_directory'] = 'inicijalizirati Directory';
+$lang['samba_directory_password_must_be_7_or_more_characters'] = 'Lozinka mora biti 7 ili više znakova.';
+$lang['samba_directory_samba_directory_driver_description'] = 'Samba vozač za usluge direktorija';
+$lang['samba_directory_samba_directory_driver'] = 'Samba Directory Driver';
+$lang['samba_directory_username_only'] = 'Korisničko Samo';
+$lang['samba_directory_with_domain'] = 'sa Domain';
