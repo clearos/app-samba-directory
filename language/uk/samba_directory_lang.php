da90eb73eeeb0a31632a3347c55455b87dbe7d3d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 04:51:48 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/uk/samba_directory_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['samba_directory_app_description'] = 'Додаток Samba Довідник надає необхідні інструменти для користувачів, груп, рахунків та інших служб каталогів.';
+$lang['samba_directory_app_name'] = 'Samba Каталог';
+$lang['samba_directory_beta_warning'] = 'Samba Каталог додаток є бета!';
+$lang['samba_directory_domain_invalid'] = 'Домен на Samba є недійсним.';
+$lang['samba_directory_initialize_directory'] = 'ініціалізувати каталог';
+$lang['samba_directory_password_must_be_7_or_more_characters'] = 'Пароль повинен бути 7 або більше символів.';
+$lang['samba_directory_samba_directory_driver_description'] = 'Драйвер Samba для служб каталогів';
+$lang['samba_directory_samba_directory_driver'] = 'Драйвер Samba Каталог';
+$lang['samba_directory_username_only'] = 'тільки ім&#39;я користувача';
+$lang['samba_directory_with_domain'] = 'З домену';
