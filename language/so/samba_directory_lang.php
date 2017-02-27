fffee1463bfe341668bc81bc7e5d6be8c4ca85e8
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 04:49:49 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/so/samba_directory_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['samba_directory_app_description'] = 'The app Samba Directory bixisaa qalabka lagama maarmaanka u ah dadka isticmaala, kooxaha, xisaab iyo adeegyada kale ee buugga.';
+$lang['samba_directory_app_name'] = 'Samba Directory';
+$lang['samba_directory_beta_warning'] = 'The Samba Directory app waa beta!';
+$lang['samba_directory_domain_invalid'] = 'Samba Domain waa baadil.';
+$lang['samba_directory_initialize_directory'] = 'initialize Directory';
+$lang['samba_directory_password_must_be_7_or_more_characters'] = 'Password waa in ay ahaadaan ama ka badan 7 characters.';
+$lang['samba_directory_samba_directory_driver_description'] = 'darawalka Samba ayaa adeegyada tusaha';
+$lang['samba_directory_samba_directory_driver'] = 'Samba Directory Driver';
+$lang['samba_directory_username_only'] = 'username Kaliya';
+$lang['samba_directory_with_domain'] = 'Iyadoo Domain';
