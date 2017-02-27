34efa1dc97b8c74d39fd7e06bce73ae395f7d92a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 04:47:04 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mn/samba_directory_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['samba_directory_app_description'] = 'Samba лавлах апп хэрэглэгч, бүлэг, данс, бусад сангийн үйлчилгээнд шаардлагатай багаж хэрэгсэл болно.';
+$lang['samba_directory_app_name'] = 'Samba лавлах';
+$lang['samba_directory_beta_warning'] = 'Samba лавлах апп бета байна!';
+$lang['samba_directory_domain_invalid'] = 'Samba Домэйн хүчин төгөлдөр бус байна.';
+$lang['samba_directory_initialize_directory'] = 'санг эхлүүлэх';
+$lang['samba_directory_password_must_be_7_or_more_characters'] = 'Нууц үг 7 буюу түүнээс дээш тэмдэгт байх ёстой.';
+$lang['samba_directory_samba_directory_driver_description'] = 'лавлах үйлчилгээ Samba драйвер';
+$lang['samba_directory_samba_directory_driver'] = 'Samba лавлах жолооч';
+$lang['samba_directory_username_only'] = 'Хэрэглэгчийн нэр Зөвхөн';
+$lang['samba_directory_with_domain'] = 'домэйнд';
