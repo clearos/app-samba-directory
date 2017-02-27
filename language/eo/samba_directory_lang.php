78190745467690319bc7f0b481e0abc6019e3b99
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 04:42:04 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/eo/samba_directory_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['samba_directory_app_description'] = 'La Samba Katalogo app provizas la necesajn ilojn por uzantoj, grupoj, raportoj kaj aliaj dosierujo servoj.';
+$lang['samba_directory_app_name'] = 'sambo Gvidlibro';
+$lang['samba_directory_beta_warning'] = 'La Samba Katalogo app estas beta!';
+$lang['samba_directory_domain_invalid'] = 'Sambo Domajno estas nevalida.';
+$lang['samba_directory_initialize_directory'] = 'pravalorizi Gvidlibro';
+$lang['samba_directory_password_must_be_7_or_more_characters'] = 'Pasvorto devas esti 7 aŭ pli karakteroj.';
+$lang['samba_directory_samba_directory_driver_description'] = 'La Samba ŝoforo por dosierujo servoj';
+$lang['samba_directory_samba_directory_driver'] = 'Sambo Gvidlibro Ŝoforo';
+$lang['samba_directory_username_only'] = 'uzantonomo Nur';
+$lang['samba_directory_with_domain'] = 'kun Domajno';
