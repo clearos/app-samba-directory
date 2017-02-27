bde4d494ff4524904121f621ae04decb6d42b5f3
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 04:51:08 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/tg/samba_directory_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['samba_directory_app_description'] = 'Дар барнома Samba Directory барои истифодабарандагон, гурӯҳҳо, ҳисобҳо ва дигар хизматрасониҳои феҳрист таъмин асбобҳои зарурӣ.';
+$lang['samba_directory_app_name'] = 'Directory Samba';
+$lang['samba_directory_beta_warning'] = 'Дар Directory барнома Samba бета аст!';
+$lang['samba_directory_domain_invalid'] = 'Samba Domain нодуруст аст.';
+$lang['samba_directory_initialize_directory'] = 'Initialize Directory';
+$lang['samba_directory_password_must_be_7_or_more_characters'] = 'Парол бояд аз 7 ё аломат бошад.';
+$lang['samba_directory_samba_directory_driver_description'] = 'Ронанда Samba барои хизматрасонии феҳристи';
+$lang['samba_directory_samba_directory_driver'] = 'Ронанда Samba Directory';
+$lang['samba_directory_username_only'] = 'Логин Танҳо';
+$lang['samba_directory_with_domain'] = 'Бо Domain';
