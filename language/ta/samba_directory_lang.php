f3c31a8711847846c5d95ebce41294ab93d7f6ec
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 04:50:48 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ta/samba_directory_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['samba_directory_app_description'] = 'சம்பா அடைவு பயன்பாட்டை பயனர்கள், குழுக்கள், கணக்குகள் மற்றும் பிற அடைவு சேவைகளுக்கு அவசியமான கருவிகள் வழங்குகிறது.';
+$lang['samba_directory_app_name'] = 'சம்பா ஏற்கனவே';
+$lang['samba_directory_beta_warning'] = 'சம்பா அடைவு பயன்பாட்டை பீட்டா உள்ளது!';
+$lang['samba_directory_domain_invalid'] = 'சம்பா களம் தவறானது.';
+$lang['samba_directory_initialize_directory'] = 'ஏற்கனவே ஆரம்பிக்கவும்';
+$lang['samba_directory_password_must_be_7_or_more_characters'] = 'கடவுச்சொல் 7 அல்லது அதற்கு மேற்பட்ட எழுத்துக்கள் இருக்க வேண்டும்.';
+$lang['samba_directory_samba_directory_driver_description'] = 'அடைவு சேவைகள் சம்பா இயக்கி';
+$lang['samba_directory_samba_directory_driver'] = 'சம்பா ஏற்கனவே டிரைவர்';
+$lang['samba_directory_username_only'] = 'பயனர் பெயர் மட்டும்';
+$lang['samba_directory_with_domain'] = 'டொமைன் மூலம்';
