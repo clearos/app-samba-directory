399107b75536e67b3722e719c8c4ac046b1c44e9
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 04:48:01 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/no/samba_directory_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['samba_directory_app_description'] = 'Samba Directory app gir de nødvendige verktøy for brukere, grupper, regnskap og andre katalogtjenester.';
+$lang['samba_directory_app_name'] = 'Samba Directory';
+$lang['samba_directory_beta_warning'] = 'Samba Directory app er beta!';
+$lang['samba_directory_domain_invalid'] = 'Samba Domain er ugyldig.';
+$lang['samba_directory_initialize_directory'] = 'initial~~POS=TRUNC Directory';
+$lang['samba_directory_password_must_be_7_or_more_characters'] = 'Passordet må være 7 eller flere tegn.';
+$lang['samba_directory_samba_directory_driver_description'] = 'Samba driver for katalogtjenester';
+$lang['samba_directory_samba_directory_driver'] = 'Samba Directory Driver';
+$lang['samba_directory_username_only'] = 'Brukernavn Only';
+$lang['samba_directory_with_domain'] = 'med Domain';
