dbc00ad31a1925324a1b206b4cd8d177f089ae93
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 04:46:25 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mg/samba_directory_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['samba_directory_app_description'] = 'Ny Samba Directory fampiharana manome ny fitaovana ilaina ho an&#39;ny mpampiasa, vondrona, tantara sy ny asa Directory hafa.';
+$lang['samba_directory_app_name'] = 'Samba Directory';
+$lang['samba_directory_beta_warning'] = 'Ny Samba Directory fampiharana no beta!';
+$lang['samba_directory_domain_invalid'] = 'Samba Domain dia tsy manan-kery.';
+$lang['samba_directory_initialize_directory'] = 'Initialize Directory';
+$lang['samba_directory_password_must_be_7_or_more_characters'] = 'Password maintsy ho 7 na mihoatra tarehin-tsoratra.';
+$lang['samba_directory_samba_directory_driver_description'] = 'Ny Samba mpamily ny Directory asa';
+$lang['samba_directory_samba_directory_driver'] = 'Samba Directory Driver';
+$lang['samba_directory_username_only'] = 'Username ihany';
+$lang['samba_directory_with_domain'] = 'miaraka Domain';
