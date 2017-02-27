e6d89bfc25e3d92de2b5281c7afd2bf076ccd157
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 04:46:35 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mi/samba_directory_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['samba_directory_app_description'] = 'Te taupānga Samba Whaiaronga whakarato te taputapu e tika ana mō ngā kaiwhakamahi, ngā rōpū, ngā pūkete, me te tahi atu ratonga whaiaronga.';
+$lang['samba_directory_app_name'] = 'Samba Whaiaronga';
+$lang['samba_directory_beta_warning'] = 'Te Samba Whaiaronga taupānga he peta!';
+$lang['samba_directory_domain_invalid'] = 'He muhu Samba Domain.';
+$lang['samba_directory_initialize_directory'] = 'arawhiti Whaiaronga';
+$lang['samba_directory_password_must_be_7_or_more_characters'] = 'Me kupuhipa 7 atu ranei pūāhua.';
+$lang['samba_directory_samba_directory_driver_description'] = 'Te taraiwa Samba mō ngā ratonga whaiaronga';
+$lang['samba_directory_samba_directory_driver'] = 'Atekōkiri Samba Whaiaronga';
+$lang['samba_directory_username_only'] = 'ingoa kaiwhakamahi anake';
+$lang['samba_directory_with_domain'] = 'ki te Domain';
