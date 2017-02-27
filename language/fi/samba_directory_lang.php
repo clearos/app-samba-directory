fe46f387bbe6041b58c2e195d3d451d012bd54af
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 04:42:26 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/fi/samba_directory_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['samba_directory_app_description'] = 'Samba Directory sovellus tarjoaa tarvittavat työkalut käyttäjien, ryhmien tilit ja muut hakemistopalvelut.';
+$lang['samba_directory_app_name'] = 'Samba hakemisto';
+$lang['samba_directory_beta_warning'] = 'Samba Directory sovellus on beta!';
+$lang['samba_directory_domain_invalid'] = 'Samba Domain on virheellinen.';
+$lang['samba_directory_initialize_directory'] = 'Alusta hakemisto';
+$lang['samba_directory_password_must_be_7_or_more_characters'] = 'Salasanan on oltava vähintään 7 merkkiä.';
+$lang['samba_directory_samba_directory_driver_description'] = 'Samba-ohjain hakemistopalvelut';
+$lang['samba_directory_samba_directory_driver'] = 'Samba Directory Kuljettajan';
+$lang['samba_directory_username_only'] = 'Käyttäjätunnus Vain';
+$lang['samba_directory_with_domain'] = 'Domain';
