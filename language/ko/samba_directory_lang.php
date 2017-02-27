abfb08edd8faaffd6fade1d6e6526cc8b9cf6eb0
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 04:45:37 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ko/samba_directory_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['samba_directory_app_description'] = '삼바 디렉토리 앱은 사용자, 그룹, 계정 및 다른 디렉터리 서비스에 필요한 도구를 제공합니다.';
+$lang['samba_directory_app_name'] = '삼바 디렉토리';
+$lang['samba_directory_beta_warning'] = '삼바 디렉토리 응용 프로그램은 베타 버전입니다!';
+$lang['samba_directory_domain_invalid'] = 'Samba 도메인이 잘못되었습니다.';
+$lang['samba_directory_initialize_directory'] = '디렉토리를 초기화';
+$lang['samba_directory_password_must_be_7_or_more_characters'] = '비밀번호는 7 자 이상이어야합니다.';
+$lang['samba_directory_samba_directory_driver_description'] = '디렉토리 서비스에 대한 삼바 드라이버';
+$lang['samba_directory_samba_directory_driver'] = '삼바 디렉토리 드라이버';
+$lang['samba_directory_username_only'] = '이름 만';
+$lang['samba_directory_with_domain'] = '도메인으로';
