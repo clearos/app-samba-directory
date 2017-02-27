4d882b8120d8d82bd5ff23f56d8be79a108995a6
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 04:40:31 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/bn/samba_directory_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['samba_directory_app_description'] = 'সাম্বা নির্দেশিকা অ্যাপ্লিকেশন ব্যবহারকারীরা, গ্রুপ, হিসাব ও অন্যান্য সেবা নির্দেশিকা জন্য প্রয়োজনীয় সামগ্রী উপলব্ধ করা.';
+$lang['samba_directory_app_name'] = 'সাম্বা নির্দেশিকা';
+$lang['samba_directory_beta_warning'] = 'সাম্বা নির্দেশিকা অ্যাপ্লিকেশন বিটা হয়!';
+$lang['samba_directory_domain_invalid'] = 'সাম্বা ডোমেন অবৈধ.';
+$lang['samba_directory_initialize_directory'] = 'আরম্ভ নির্দেশিকা';
+$lang['samba_directory_password_must_be_7_or_more_characters'] = 'পাসওয়ার্ড 7 বা তার বেশি অক্ষরের হতে হবে.';
+$lang['samba_directory_samba_directory_driver_description'] = 'সেবা নির্দেশিকা জন্য সাম্বা চালক';
+$lang['samba_directory_samba_directory_driver'] = 'সাম্বা নির্দেশিকা ড্রাইভার';
+$lang['samba_directory_username_only'] = 'ব্যবহারকারীর নাম শুধু';
+$lang['samba_directory_with_domain'] = 'ডোমেনের সাথে';
