0ead7361268ef8166c9e73b3ae0385f75fb0ad38
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 04:41:45 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/cs/samba_directory_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['samba_directory_app_description'] = 'Aplikace Samba Directory poskytuje nezbytné nástroje pro uživatele, skupiny účtů a jiných adresářových služeb.';
+$lang['samba_directory_app_name'] = 'Samba Directory';
+$lang['samba_directory_beta_warning'] = 'Samba Adresář aplikace je beta!';
+$lang['samba_directory_domain_invalid'] = 'Samba Doména je neplatný.';
+$lang['samba_directory_initialize_directory'] = 'inicializovat adresář';
+$lang['samba_directory_password_must_be_7_or_more_characters'] = 'Heslo musí být 7 nebo více znaků.';
+$lang['samba_directory_samba_directory_driver_description'] = 'Řidič Samba pro adresářové služby';
+$lang['samba_directory_samba_directory_driver'] = 'Samba Directory Driver';
+$lang['samba_directory_username_only'] = 'Uživatelské jméno pouze';
+$lang['samba_directory_with_domain'] = 's doménou';
