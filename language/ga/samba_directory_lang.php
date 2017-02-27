dc9bc87a5bfddbae9bbe3d0cc8b1f5f93c2b072b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 04:44:10 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ga/samba_directory_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['samba_directory_app_description'] = 'Soláthraíonn an app Samba Eolaire na huirlisí is gá d&#39;úsáideoirí, grúpaí, cuntais agus seirbhísí eolaire eile.';
+$lang['samba_directory_app_name'] = 'samba Eolaire';
+$lang['samba_directory_beta_warning'] = 'Is é an Eolaire Samba app beta!';
+$lang['samba_directory_domain_invalid'] = 'Is Samba Fearann ​​neamhbhailí.';
+$lang['samba_directory_initialize_directory'] = 'thúsú Eolaire';
+$lang['samba_directory_password_must_be_7_or_more_characters'] = 'Ní mór Pasfhocal a bheith 7 nó níos carachtair.';
+$lang['samba_directory_samba_directory_driver_description'] = 'An tiománaí Samba do sheirbhísí eolaire';
+$lang['samba_directory_samba_directory_driver'] = 'Samba Eolaire Tiománaí';
+$lang['samba_directory_username_only'] = 'Ainm úsáideora Amháin';
+$lang['samba_directory_with_domain'] = 'le Fearann';
