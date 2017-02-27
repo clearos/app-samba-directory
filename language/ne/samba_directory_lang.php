5016cd14c0dbc53f2690a059b30187c0a6efea80
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 04:47:42 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ne/samba_directory_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['samba_directory_app_description'] = 'यो साम्बा निर्देशिका अनुप्रयोग प्रयोगकर्ता, समूह, खाता र अन्य निर्देशिका सेवाहरूको लागि आवश्यक उपकरण प्रदान गर्दछ।';
+$lang['samba_directory_app_name'] = 'साम्बा निर्देशिका';
+$lang['samba_directory_beta_warning'] = 'यो साम्बा निर्देशिका अनुप्रयोग बिटा छ!';
+$lang['samba_directory_domain_invalid'] = 'साम्बा डोमेन अमान्य छ।';
+$lang['samba_directory_initialize_directory'] = 'निर्देशिका सुरु';
+$lang['samba_directory_password_must_be_7_or_more_characters'] = 'पासवर्ड 7 या अधिक अक्षर हुनुपर्छ।';
+$lang['samba_directory_samba_directory_driver_description'] = 'निर्देशिका सेवाहरू लागि साम्बा चालक';
+$lang['samba_directory_samba_directory_driver'] = 'साम्बा निर्देशिका चालक';
+$lang['samba_directory_username_only'] = 'प्रयोगकर्ता नाम मात्र';
+$lang['samba_directory_with_domain'] = 'डोमेन संग';
