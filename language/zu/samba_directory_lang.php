8f6e81bbfec2cd4288cd93338887685ce0f61efd
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 04:52:58 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/zu/samba_directory_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['samba_directory_app_description'] = 'Uhlelo lokusebenza Samba Directory inikeza amathuluzi adingekayo kubasebenzisi, amaqembu, ama-akhawunti kanye nezinye izinsizakalo lwemibhalo.';
+$lang['samba_directory_app_name'] = 'Samba Directory';
+$lang['samba_directory_beta_warning'] = 'Uhlelo Samba Directory lokusebenza kuyinto beta!';
+$lang['samba_directory_domain_invalid'] = 'Samba Domain ayivumelekile.';
+$lang['samba_directory_initialize_directory'] = 'ukuqalisa Directory';
+$lang['samba_directory_password_must_be_7_or_more_characters'] = 'Iphasiwedi kumele ibe 7 noma ngaphezulu abalingiswa.';
+$lang['samba_directory_samba_directory_driver_description'] = 'I Samba umshayeli isiqondisi amasevisi';
+$lang['samba_directory_samba_directory_driver'] = 'Samba Directory Driver';
+$lang['samba_directory_username_only'] = 'Igama lomsebenzisi kuphela';
+$lang['samba_directory_with_domain'] = 'With Domain';
