e4f0de142c807ee9edef7823c0070ba668856378
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 04:46:06 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lt/samba_directory_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['samba_directory_app_description'] = 'Samba katalogo App suteikia reikiamas priemones vartotojų, grupių, sąskaitų ir kitų katalogų paslaugų.';
+$lang['samba_directory_app_name'] = 'Samba katalogas';
+$lang['samba_directory_beta_warning'] = 'Samba katalogo programa yra beta!';
+$lang['samba_directory_domain_invalid'] = 'Samba Domeno negalioja.';
+$lang['samba_directory_initialize_directory'] = 'inicijuoti katalogas';
+$lang['samba_directory_password_must_be_7_or_more_characters'] = 'Slaptažodis turi būti 7 arba daugiau simbolių.';
+$lang['samba_directory_samba_directory_driver_description'] = 'Samba vairuotojas abonentus teikimo paslaugos';
+$lang['samba_directory_samba_directory_driver'] = 'Samba katalogas Vairuotojas';
+$lang['samba_directory_username_only'] = 'Nick Tik';
+$lang['samba_directory_with_domain'] = 'domeno';
