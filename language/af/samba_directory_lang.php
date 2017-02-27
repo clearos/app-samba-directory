cc663336ddb9da201080d5aa4b3b4406842603ef
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 04:39:25 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/af/samba_directory_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['samba_directory_app_description'] = 'Die Samba Gids app bied die nodige gereedskap vir gebruikers, groepe, rekeninge en ander gids dienste.';
+$lang['samba_directory_app_name'] = 'Samba Gids';
+$lang['samba_directory_beta_warning'] = 'Die Samba Gids app is beta!';
+$lang['samba_directory_domain_invalid'] = 'Samba Domain is ongeldig.';
+$lang['samba_directory_initialize_directory'] = 'inisialiseer Gids';
+$lang['samba_directory_password_must_be_7_or_more_characters'] = 'Wagwoord moet wees 7 of meer karakters.';
+$lang['samba_directory_samba_directory_driver_description'] = 'Die Samba bestuurder vir directory services';
+$lang['samba_directory_samba_directory_driver'] = 'Samba Gids Driver';
+$lang['samba_directory_username_only'] = 'Slegs Gebruikersnaam';
+$lang['samba_directory_with_domain'] = 'met Domain';
