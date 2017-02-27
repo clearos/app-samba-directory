a3bad6032f0aadf1ed2239c881eb73d18f732565
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 04:43:22 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ht/samba_directory_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['samba_directory_app_description'] = 'App a Samba Anyè bay zouti ki nesesè pou itilizatè, gwoup, kont ak lòt sèvis anyè.';
+$lang['samba_directory_app_name'] = 'Samba Anyè';
+$lang['samba_directory_beta_warning'] = 'Samba Anyè App a se beta!';
+$lang['samba_directory_domain_invalid'] = 'Samba Domèn se valab.';
+$lang['samba_directory_initialize_directory'] = 'inisyalize Anyè';
+$lang['samba_directory_password_must_be_7_or_more_characters'] = 'Modpas dwe 7 oswa plis karaktè.';
+$lang['samba_directory_samba_directory_driver_description'] = 'Chofè a Samba pou sèvis anyè';
+$lang['samba_directory_samba_directory_driver'] = 'Samba Anyè chofè';
+$lang['samba_directory_username_only'] = 'D &#39;Se sèlman';
+$lang['samba_directory_with_domain'] = 'avèk Domèn';
