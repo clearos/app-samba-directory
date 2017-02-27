8ee9c9954e3c9f57eb0c9c9b0bc72a2676fadc63
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 04:50:09 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/es/samba_directory_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['samba_directory_app_description'] = 'La aplicación Directorio de Samba proporciona las herramientas necesarias para los usuarios, grupos, cuentas y otros servicios de directorio.';
+$lang['samba_directory_app_name'] = 'Directorio de Samba';
+$lang['samba_directory_beta_warning'] = 'El Directorio de Samba aplicación es beta!';
+$lang['samba_directory_domain_invalid'] = 'Samba de dominio no es válido.';
+$lang['samba_directory_initialize_directory'] = 'inicializar Directorio';
+$lang['samba_directory_password_must_be_7_or_more_characters'] = 'La contraseña debe ser de 7 o más caracteres.';
+$lang['samba_directory_samba_directory_driver_description'] = 'El conductor Samba para servicios de directorio';
+$lang['samba_directory_samba_directory_driver'] = 'Directorio de Samba Conductor';
+$lang['samba_directory_username_only'] = 'Sólo nombre de usuario';
+$lang['samba_directory_with_domain'] = 'con dominio';
