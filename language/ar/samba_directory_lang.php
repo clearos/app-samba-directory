479bf8891376548d98941c9ffca3c93b89ef3f7f
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 04:39:44 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ar/samba_directory_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['samba_directory_app_description'] = 'يوفر تطبيق سامبا دليل الأدوات اللازمة للمستخدمين والمجموعات والحسابات وخدمات الدليل أخرى.';
+$lang['samba_directory_app_name'] = 'دليل سامبا';
+$lang['samba_directory_beta_warning'] = 'دليل التطبيق سامبا هو بيتا!';
+$lang['samba_directory_domain_invalid'] = 'سامبا المجال غير صالح.';
+$lang['samba_directory_initialize_directory'] = 'تهيئة الدليل';
+$lang['samba_directory_password_must_be_7_or_more_characters'] = 'يجب أن تكون كلمة 7 أحرف أو أكثر.';
+$lang['samba_directory_samba_directory_driver_description'] = 'السائق سامبا لخدمات الدليل';
+$lang['samba_directory_samba_directory_driver'] = 'سائق سامبا دليل';
+$lang['samba_directory_username_only'] = 'اسم المستخدم فقط';
+$lang['samba_directory_with_domain'] = 'مع المجال';
