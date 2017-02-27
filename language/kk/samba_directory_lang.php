c7dca0ef24a1e57c8b598bcab31a8bebf0e72d04
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 04:45:08 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/kk/samba_directory_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['samba_directory_app_description'] = 'Samba Directory Қолданба пайдаланушылар, топтар, шоттар және басқа да анықтамалық қызмет үшін қажетті құралдармен қамтамасыз етеді.';
+$lang['samba_directory_app_name'] = 'Samba Directory';
+$lang['samba_directory_beta_warning'] = 'Samba Directory Қолданба бета!';
+$lang['samba_directory_domain_invalid'] = 'Samba домен жарамсыз болып табылады.';
+$lang['samba_directory_initialize_directory'] = 'Directory инициализациясы';
+$lang['samba_directory_password_must_be_7_or_more_characters'] = 'Құпия сөз 7 немесе одан да көп таңбадан тұруы тиіс.';
+$lang['samba_directory_samba_directory_driver_description'] = 'каталогтар қызметтері үшін Samba драйвер';
+$lang['samba_directory_samba_directory_driver'] = 'Samba Directory Driver';
+$lang['samba_directory_username_only'] = 'Тек Болмайды';
+$lang['samba_directory_with_domain'] = 'доменімен';
