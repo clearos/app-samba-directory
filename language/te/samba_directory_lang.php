13d82ba3085c9d71c92b41d59353f7b35f217664
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 04:50:58 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/te/samba_directory_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['samba_directory_app_description'] = 'సాంబా డైరెక్టరీ అనువర్తనం వినియోగదారులు, సమూహాలు, ఖాతాలు మరియు ఇతర డైరెక్టరీ సేవలకు అవసరమైన సాధనాలను అందించును.';
+$lang['samba_directory_app_name'] = 'సాంబా డైరెక్టరీ';
+$lang['samba_directory_beta_warning'] = 'సాంబా డైరెక్టరీ అనువర్తనం బీటా!';
+$lang['samba_directory_domain_invalid'] = 'సాంబా డొమైన్ చెల్లదు.';
+$lang['samba_directory_initialize_directory'] = 'డైరెక్టరీ ప్రారంభించును';
+$lang['samba_directory_password_must_be_7_or_more_characters'] = 'పాస్వర్డ్ 7 లేదా ఎక్కువ అక్షరాలు ఉండాలి.';
+$lang['samba_directory_samba_directory_driver_description'] = 'డైరెక్టరీ సేవలు సాంబా డ్రైవర్';
+$lang['samba_directory_samba_directory_driver'] = 'సాంబా డైరెక్టరీ డ్రైవర్';
+$lang['samba_directory_username_only'] = 'యూజర్ పేరు మాత్రమే';
+$lang['samba_directory_with_domain'] = 'డొమైన్ తో';
