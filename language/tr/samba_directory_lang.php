2d885af406c96aabad2d0272017059d3930a61c0
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 04:51:38 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/tr/samba_directory_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['samba_directory_app_description'] = 'Samba Dizini uygulaması, kullanıcıların, grupların, hesaplar ve diğer dizin hizmetleri için gerekli araçları sağlar.';
+$lang['samba_directory_app_name'] = 'Samba Rehberi';
+$lang['samba_directory_beta_warning'] = 'Samba Dizin uygulaması beta!';
+$lang['samba_directory_domain_invalid'] = 'Samba Domain geçersiz.';
+$lang['samba_directory_initialize_directory'] = 'Dizin başlat';
+$lang['samba_directory_password_must_be_7_or_more_characters'] = 'Şifre 7 veya daha fazla karakter olmalıdır.';
+$lang['samba_directory_samba_directory_driver_description'] = 'dizin hizmetleri için Samba sürücüsü';
+$lang['samba_directory_samba_directory_driver'] = 'Samba Dizin Sürücü';
+$lang['samba_directory_username_only'] = 'Adı Sadece';
+$lang['samba_directory_with_domain'] = 'Domain ile';
