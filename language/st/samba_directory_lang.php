5f639f6dcbddb6888b21c87750e0d4ac81af420e
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 04:49:58 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/st/samba_directory_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['samba_directory_app_description'] = 'The app e Samba Directory fana ka lisebelisoa tsa sona tse hlokehang bakeng sa basebedisi, lihlopha, litlaleho le litšebeletso tse ling dibaka.';
+$lang['samba_directory_app_name'] = 'Samba Directory';
+$lang['samba_directory_beta_warning'] = 'The app e Samba Directory e, beta!';
+$lang['samba_directory_domain_invalid'] = 'Samba Domain ha o sebetse.';
+$lang['samba_directory_initialize_directory'] = 'Initialize Directory';
+$lang['samba_directory_password_must_be_7_or_more_characters'] = 'Password lokela ho batho bao ho buuoang 7 kapa ho feta.';
+$lang['samba_directory_samba_directory_driver_description'] = 'Mokhanni Samba bakeng sa litšebeletso dibaka';
+$lang['samba_directory_samba_directory_driver'] = 'Samba Directory Driver';
+$lang['samba_directory_username_only'] = 'Username feela';
+$lang['samba_directory_with_domain'] = 'le Domain';
