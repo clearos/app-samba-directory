d7fd715be01574e532ff10140060b7cb8d7d1451
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 04:52:18 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/vi/samba_directory_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['samba_directory_app_description'] = 'Các ứng dụng Samba Directory cung cấp những công cụ cần thiết cho người dùng, các nhóm, các tài khoản và các dịch vụ thư mục khác.';
+$lang['samba_directory_app_name'] = 'Samba mục';
+$lang['samba_directory_beta_warning'] = 'Các thư mục ứng dụng Samba là phiên bản beta!';
+$lang['samba_directory_domain_invalid'] = 'Samba miền là không hợp lệ.';
+$lang['samba_directory_initialize_directory'] = 'khởi tạo thư mục';
+$lang['samba_directory_password_must_be_7_or_more_characters'] = 'Mật khẩu phải có 7 ký tự.';
+$lang['samba_directory_samba_directory_driver_description'] = 'Người lái xe Samba cho các dịch vụ thư mục';
+$lang['samba_directory_samba_directory_driver'] = 'Samba mục driver';
+$lang['samba_directory_username_only'] = 'Chỉ có Tên đăng nhập';
+$lang['samba_directory_with_domain'] = 'với tên miền';
