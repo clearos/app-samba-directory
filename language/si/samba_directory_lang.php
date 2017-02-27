52dac5a39dfe5a526a612f4af03d79d0ac01eff1
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 04:49:19 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/si/samba_directory_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['samba_directory_app_description'] = 'මෙම සම්බා නාමාවලිය යෙදුම පරිශීලකයන්, කණ්ඩායම්, ගිණුම් හා වෙනත් බහලුම සේවා සඳහා අවශ්ය මෙවලම් සපයයි.';
+$lang['samba_directory_app_name'] = 'සම්බා නාමාවලිය';
+$lang['samba_directory_beta_warning'] = 'මෙම සම්බා නාමාවලිය යෙදුම බීටා වේ!';
+$lang['samba_directory_domain_invalid'] = 'සම්බා වසම් වලංගු නැත.';
+$lang['samba_directory_initialize_directory'] = 'නාමාවලිය ආරම්භනය';
+$lang['samba_directory_password_must_be_7_or_more_characters'] = 'රහස් පදය අක්ෂර 7 හෝ ඊට වැඩි විය යුතුය.';
+$lang['samba_directory_samba_directory_driver_description'] = 'බහලුම සේවා සඳහා සම්බා රියදුරු';
+$lang['samba_directory_samba_directory_driver'] = 'සම්බා නාමාවලිය ධාවක';
+$lang['samba_directory_username_only'] = 'පරිශීලක නාමය පමණක්';
+$lang['samba_directory_with_domain'] = 'වසම් සමඟ';
