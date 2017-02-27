405621d12387d1b383e44c1a803e0e2c6c218459
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 04:44:29 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/is/samba_directory_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['samba_directory_app_description'] = 'Samba Directory app veitir nauðsynlegar verkfæri fyrir notendur, hópa reikninga og önnur skrá þjónustu.';
+$lang['samba_directory_app_name'] = 'samba Directory';
+$lang['samba_directory_beta_warning'] = 'Samba Directory app er beta!';
+$lang['samba_directory_domain_invalid'] = 'Samba Domain er ógilt.';
+$lang['samba_directory_initialize_directory'] = 'frumstilla Directory';
+$lang['samba_directory_password_must_be_7_or_more_characters'] = 'Lykilorð verður að vera 7 eða fleiri stafi.';
+$lang['samba_directory_samba_directory_driver_description'] = 'Samba Ökumaðurinn fyrir þjónustu skrá';
+$lang['samba_directory_samba_directory_driver'] = 'Samba Directory Driver';
+$lang['samba_directory_username_only'] = 'Notandanafn Aðeins';
+$lang['samba_directory_with_domain'] = 'með Domain';
