9aa656028bc2b49c700073a3ffd1d9838f4c66c1
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 04:52:08 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/uz/samba_directory_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['samba_directory_app_description'] = 'Samba Directory ilova foydalanuvchilar, guruhlar, hisob va boshqa katalog xizmatlar uchun zarur vositalarni beradi.';
+$lang['samba_directory_app_name'] = 'Samba Directory';
+$lang['samba_directory_beta_warning'] = 'Samba Directory ilova beta hisoblanadi!';
+$lang['samba_directory_domain_invalid'] = 'Samba Domen haqiqiy emas.';
+$lang['samba_directory_initialize_directory'] = 'Directory boshla';
+$lang['samba_directory_password_must_be_7_or_more_characters'] = 'Parol 7 yoki bir necha belgilar bo&#39;lishi kerak.';
+$lang['samba_directory_samba_directory_driver_description'] = 'katalog xizmatlari uchun Samba haydovchisi';
+$lang['samba_directory_samba_directory_driver'] = 'Samba Directory Driver';
+$lang['samba_directory_username_only'] = 'Login Faqat';
+$lang['samba_directory_with_domain'] = 'domeni bilan';
