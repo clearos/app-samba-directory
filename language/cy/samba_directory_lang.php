647ef6f44af446100cf27ff3422842245c2583bb
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 04:52:28 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/cy/samba_directory_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['samba_directory_app_description'] = 'Mae&#39;r app Cyfeiriadur Samba darparu&#39;r offer angenrheidiol ar gyfer defnyddwyr, grwpiau, cyfrifon a gwasanaethau cyfeiriadur eraill.';
+$lang['samba_directory_app_name'] = 'Cyfeiriadur Samba';
+$lang['samba_directory_beta_warning'] = 'Mae&#39;r Cyfeirlyfr Samba app yn beta!';
+$lang['samba_directory_domain_invalid'] = 'Samba Parth yn annilys.';
+$lang['samba_directory_initialize_directory'] = 'ymgychwyn Cyfeiriadur';
+$lang['samba_directory_password_must_be_7_or_more_characters'] = 'Rhaid i&#39;r cyfrinair fod yn 7 neu fwy o gymeriadau.';
+$lang['samba_directory_samba_directory_driver_description'] = 'Mae&#39;r gyrrwr Samba ar gyfer gwasanaethau cyfeiriadur';
+$lang['samba_directory_samba_directory_driver'] = 'Cyfeiriadur Samba Driver';
+$lang['samba_directory_username_only'] = 'enw defnyddiwr yn Unig';
+$lang['samba_directory_with_domain'] = 'gyda Parth';
