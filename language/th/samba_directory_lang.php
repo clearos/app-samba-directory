2b9c034996a9b0c381901d88777323d07676f420
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 04:51:18 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/th/samba_directory_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['samba_directory_app_description'] = 'แอปพลิเคแซมบ้าไดเรกทอรีที่มีเครื่องมือที่จำเป็นสำหรับผู้ใช้กลุ่มบัญชีและบริการไดเรกทอรีอื่น ๆ';
+$lang['samba_directory_app_name'] = 'แซมบ้าไดเรกทอรี';
+$lang['samba_directory_beta_warning'] = 'แซมบ้าไดเรกทอรี app เป็นเบต้า!';
+$lang['samba_directory_domain_invalid'] = 'แซมบ้าโดเมนไม่ถูกต้อง';
+$lang['samba_directory_initialize_directory'] = 'เริ่มต้นไดเรกทอรี';
+$lang['samba_directory_password_must_be_7_or_more_characters'] = 'รหัสผ่านต้องมี 7 หรือมากกว่าตัวอักษร';
+$lang['samba_directory_samba_directory_driver_description'] = 'คนขับแซมบ้าสำหรับการให้บริการไดเรกทอรี';
+$lang['samba_directory_samba_directory_driver'] = 'ไดร์เวอร์แซมบ้าไดเรกทอรี';
+$lang['samba_directory_username_only'] = 'ชื่อผู้ใช้เท่านั้น';
+$lang['samba_directory_with_domain'] = 'กับโดเมน';
