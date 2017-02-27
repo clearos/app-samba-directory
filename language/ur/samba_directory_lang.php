12547fd7346f1ec7d7610623ffd579cd7b9bb3e0
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 04:51:58 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ur/samba_directory_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['samba_directory_app_description'] = 'سامبا ڈائرکٹری اپلی کیشن صارفین، گروپوں، اکاؤنٹس اور دیگر ڈائرکٹری کی خدمات کے لئے ضروری آلات فراہم کرتا ہے.';
+$lang['samba_directory_app_name'] = 'سامبا ڈائرکٹری';
+$lang['samba_directory_beta_warning'] = 'سامبا ڈائریکٹری اے پی پی بیٹا ہے!';
+$lang['samba_directory_domain_invalid'] = 'سامبا ڈومین غلط ہے.';
+$lang['samba_directory_initialize_directory'] = 'ڈائرکٹری کی ابتدا';
+$lang['samba_directory_password_must_be_7_or_more_characters'] = 'پاس ورڈ 7 یا اس سے زیادہ حروف کا ہونا ضروری ہے.';
+$lang['samba_directory_samba_directory_driver_description'] = 'کے لئے ڈائریکٹری خدمات سامبا ڈرائیور';
+$lang['samba_directory_samba_directory_driver'] = 'سامبا ڈائرکٹری ڈرائیور';
+$lang['samba_directory_username_only'] = 'صارف کا نام صرف';
+$lang['samba_directory_with_domain'] = 'ڈومین کے ساتھ';
