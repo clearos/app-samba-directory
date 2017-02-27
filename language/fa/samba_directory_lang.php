df572c151d75bcb19c47fac01a5ce57dd441715a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 04:48:20 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/fa/samba_directory_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['samba_directory_app_description'] = 'برنامه سامبا راهنمای برای کاربران، گروه، حساب و سایر خدمات دایرکتوری فراهم می کند ابزار لازم است.';
+$lang['samba_directory_app_name'] = 'راهنمای سامبا';
+$lang['samba_directory_beta_warning'] = 'راهنمای برنامه سامبا بتا است!';
+$lang['samba_directory_domain_invalid'] = 'سامبا دامنه نامعتبر است.';
+$lang['samba_directory_initialize_directory'] = 'مقداردهی اولیه کتاب راهنمای';
+$lang['samba_directory_password_must_be_7_or_more_characters'] = 'رمز عبور باید 7 یا بیشتر کاراکتر باشد.';
+$lang['samba_directory_samba_directory_driver_description'] = 'راننده سامبا برای خدمات دایرکتوری';
+$lang['samba_directory_samba_directory_driver'] = 'درایور سامبا راهنمای';
+$lang['samba_directory_username_only'] = 'نام کاربری فقط';
+$lang['samba_directory_with_domain'] = 'با دامنه';
