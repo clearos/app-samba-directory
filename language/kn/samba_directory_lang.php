846dd3a3c61944b9e5e6d10f3007116745d8d9f9
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 04:45:27 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/kn/samba_directory_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['samba_directory_app_description'] = 'ಸಾಂಬಾ ಡೈರೆಕ್ಟರಿ ಅಪ್ಲಿಕೇಶನ್ ಬಳಕೆದಾರರು, ಸಮೂಹಗಳು, ಖಾತೆಗಳನ್ನು ಮತ್ತು ಇತರ ಕೈಪಿಡಿ ಸೇವೆಗಳಿಗೆ ಅಗತ್ಯವಿರುವ ಉಪಕರಣವನ್ನು ಒದಗಿಸುತ್ತದೆ.';
+$lang['samba_directory_app_name'] = 'ಸಾಂಬಾ ಡೈರೆಕ್ಟರಿ';
+$lang['samba_directory_beta_warning'] = 'ಸಾಂಬಾ ಡೈರೆಕ್ಟರಿ ಅಪ್ಲಿಕೇಶನ್ ಬೀಟಾ ಆಗಿದೆ!';
+$lang['samba_directory_domain_invalid'] = 'ಸಾಂಬಾ ಡೊಮೈನ್ ಅಮಾನ್ಯವಾಗಿದೆ.';
+$lang['samba_directory_initialize_directory'] = 'ಕೋಶ &#39;';
+$lang['samba_directory_password_must_be_7_or_more_characters'] = 'ಪಾಸ್ವರ್ಡ್ 7 ಅಥವಾ ಹೆಚ್ಚು ಪಾತ್ರಗಳು ಇರಬೇಕು.';
+$lang['samba_directory_samba_directory_driver_description'] = 'ಕೈಪಿಡಿ ಸೇವೆಗಳಿಗೆ ಸಾಂಬಾ ಚಾಲಕ';
+$lang['samba_directory_samba_directory_driver'] = 'ಸಾಂಬಾ ಡೈರೆಕ್ಟರಿ ಚಾಲಕ';
+$lang['samba_directory_username_only'] = 'ಬಳಕೆದಾರ ಹೆಸರು ಮಾತ್ರ';
+$lang['samba_directory_with_domain'] = 'ಡೊಮೇನ್';
