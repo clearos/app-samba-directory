fd4f15f3f91e88ef585ce42eac33507da36c5017
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 04:40:59 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/my/samba_directory_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['samba_directory_app_description'] = 'အဆိုပါ Samba Directory ကို app ကိုအသုံးပြုသူများအဖွဲ့များ, အကောင့်များနှင့်အခြား directory ကိုန်ဆောင်မှုများအတွက်လိုအပ်သော tools တွေကိုပေးစွမ်းသည်။';
+$lang['samba_directory_app_name'] = 'Samba Directory ကို';
+$lang['samba_directory_beta_warning'] = 'အဆိုပါ Samba Directory ကို app ကို beta ကိုပါ!';
+$lang['samba_directory_domain_invalid'] = 'Samba ဒိုမိန်းမမှန်ကန်ဘူး။';
+$lang['samba_directory_initialize_directory'] = 'Directory ကိုစတငျ';
+$lang['samba_directory_password_must_be_7_or_more_characters'] = 'Password ကို 7 သို့မဟုတ်ထိုထက်ပိုဇာတ်ကောင်ဖြစ်ရပါမည်။';
+$lang['samba_directory_samba_directory_driver_description'] = 'directory ကိုန်ဆောင်မှုများအတွက် Samba ကားမောင်းသူ';
+$lang['samba_directory_samba_directory_driver'] = 'Samba Directory ကိုယာဉ်မောင်း';
+$lang['samba_directory_username_only'] = 'ကသာအသုံးပြုသူအမည်';
+$lang['samba_directory_with_domain'] = 'ဒိုမိန်းနှင့်အတူ';
