5f84f73b2617c94ee61e68f759c28b34121e2d9e
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 04:41:08 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ca/samba_directory_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['samba_directory_app_description'] = 'L&#39;aplicació Directori de Samba proporciona les eines necessàries per als usuaris, grups, comptes i altres serveis de directori.';
+$lang['samba_directory_app_name'] = 'Directori de Samba';
+$lang['samba_directory_beta_warning'] = 'El Directori de Samba aplicació és beta!';
+$lang['samba_directory_domain_invalid'] = 'Samba de domini no és vàlid.';
+$lang['samba_directory_initialize_directory'] = 'inicialitzar Directori';
+$lang['samba_directory_password_must_be_7_or_more_characters'] = 'La contrasenya ha de ser de 7 o més caràcters.';
+$lang['samba_directory_samba_directory_driver_description'] = 'El conductor Samba per a serveis de directori';
+$lang['samba_directory_samba_directory_driver'] = 'Directori de Samba Conductor';
+$lang['samba_directory_username_only'] = 'Només nom d&#39;usuari';
+$lang['samba_directory_with_domain'] = 'amb domini';
