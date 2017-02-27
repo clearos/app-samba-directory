c06ce64eaa9752c3726b4ce9e10adc5d4b114155
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 04:46:54 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ml/samba_directory_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['samba_directory_app_description'] = 'സാംബാ ഡയറക്ടറി അപ്ലിക്കേഷൻ ഉപയോക്താക്കൾ, ഗ്രൂപ്പുകൾ, അക്കൌണ്ടുകളും മറ്റ് ഡയറക്ടറി സേവനങ്ങൾ ആവശ്യമായ ഉപകരണങ്ങൾ നൽകുന്നു.';
+$lang['samba_directory_app_name'] = 'സാംബാ ഡയറക്ടറി';
+$lang['samba_directory_beta_warning'] = 'സാംബാ ഡയറക്ടറി അപ്ലിക്കേഷൻ ബീറ്റ ആണ്!';
+$lang['samba_directory_domain_invalid'] = 'സാംബാ ഡൊമെയ്ൻ അസാധുവാണ്.';
+$lang['samba_directory_initialize_directory'] = 'ഡയറക്ടറി സമാരംഭിക്കുക';
+$lang['samba_directory_password_must_be_7_or_more_characters'] = 'പാസ്വേഡ് 7 അല്ലെങ്കിൽ കൂടുതൽ പ്രതീകങ്ങൾ ആയിരിക്കണം.';
+$lang['samba_directory_samba_directory_driver_description'] = 'ഡയറക്ടറി സേവനങ്ങൾക്കായി സാംബാ ഡ്രൈവർ';
+$lang['samba_directory_samba_directory_driver'] = 'സാംബാ ഡയറക്ടറി ഡ്രൈവർ';
+$lang['samba_directory_username_only'] = 'മാത്രം ഉപയോക്തൃനാമം';
+$lang['samba_directory_with_domain'] = 'ഡൊമെയ്ൻ ഉപയോഗിച്ച്';
