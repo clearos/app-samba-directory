c5c2d4aaefb1fc78662411793a4fa4a44b1e09f1
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 04:47:23 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ms/samba_directory_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['samba_directory_app_description'] = 'Aplikasi Direktori Samba menyediakan alat yang diperlukan untuk pengguna, kumpulan, akaun dan perkhidmatan direktori lain.';
+$lang['samba_directory_app_name'] = 'Direktori Samba';
+$lang['samba_directory_beta_warning'] = 'The Samba app Directory ialah beta!';
+$lang['samba_directory_domain_invalid'] = 'Samba Domain tidak sah.';
+$lang['samba_directory_initialize_directory'] = 'memulakan Direktori';
+$lang['samba_directory_password_must_be_7_or_more_characters'] = 'Kata laluan mesti mengandungi 7 atau lebih aksara.';
+$lang['samba_directory_samba_directory_driver_description'] = 'Pemandu Samba untuk perkhidmatan direktori';
+$lang['samba_directory_samba_directory_driver'] = 'Direktori Samba pemandu';
+$lang['samba_directory_username_only'] = 'Nama pengguna Hanya';
+$lang['samba_directory_with_domain'] = 'dengan Domain';
