f6575e313dc37e6470c32dac730322c85a3def00
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 04:45:18 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/km/samba_directory_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['samba_directory_app_description'] = 'កម្មវិធីថតផ្តល់នូវឧបករណ៍ចាំបាច់របស់ Samba នេះសម្រាប់អ្នកប្រើប្រាស់, ក្រុម, គណនីនិងសេវាកម្មថតផ្សេងទៀត។';
+$lang['samba_directory_app_name'] = 'ថត Samba & ‧;';
+$lang['samba_directory_beta_warning'] = 'នេះគឺជាកម្មវិធីរបស់ Samba ថតបេតា!';
+$lang['samba_directory_domain_invalid'] = 'ដែនរបស់ Samba មិនត្រឹមត្រូវ។';
+$lang['samba_directory_initialize_directory'] = 'ចាប់ផ្ដើមថត';
+$lang['samba_directory_password_must_be_7_or_more_characters'] = 'ពាក្យសម្ងាត់ត្រូវតែ 7 ឬច្រើនជាងតួអក្សរ។';
+$lang['samba_directory_samba_directory_driver_description'] = 'កម្មវិធីបញ្ជារបស់ Samba សម្រាប់សេវាថត';
+$lang['samba_directory_samba_directory_driver'] = 'កម្មវិធីបញ្ជាថត Samba & ‧;';
+$lang['samba_directory_username_only'] = 'ឈ្មោះអ្នកប្រើតែ';
+$lang['samba_directory_with_domain'] = 'ជាមួយដែន';
