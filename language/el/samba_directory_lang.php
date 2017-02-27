8e71dfae7a58296dc018308083b5b8328f845a53
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 04:43:03 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/el/samba_directory_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['samba_directory_app_description'] = 'Η εφαρμογή Samba Directory παρέχει τα απαραίτητα εργαλεία για τους χρήστες, τις ομάδες, τους λογαριασμούς και άλλες υπηρεσίες καταλόγου.';
+$lang['samba_directory_app_name'] = 'Samba Directory';
+$lang['samba_directory_beta_warning'] = 'Ο κατάλογος Samba app είναι beta!';
+$lang['samba_directory_domain_invalid'] = 'Samba τομέα δεν είναι έγκυρο.';
+$lang['samba_directory_initialize_directory'] = 'αρχικοποίηση Directory';
+$lang['samba_directory_password_must_be_7_or_more_characters'] = 'Ο κωδικός πρόσβασης πρέπει να είναι 7 ή περισσότερους χαρακτήρες.';
+$lang['samba_directory_samba_directory_driver_description'] = 'Ο οδηγός Samba για υπηρεσίες καταλόγου';
+$lang['samba_directory_samba_directory_driver'] = 'Samba Directory Driver';
+$lang['samba_directory_username_only'] = 'Όνομα Χρήστη Μόνο';
+$lang['samba_directory_with_domain'] = 'με το Domain';
