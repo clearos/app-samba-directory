61f7a597e2e39928f6439b493bb60eb266dd7e88
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 04:50:19 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/su/samba_directory_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['samba_directory_app_description'] = 'Nu Samba Diréktori app nyadiakeun pakakas dipikabutuh pikeun pamaké, kelompok, akun sareng ladenan diréktori séjén.';
+$lang['samba_directory_app_name'] = 'Diréktori Samba';
+$lang['samba_directory_beta_warning'] = 'Nu Samba Diréktori app téh béta!';
+$lang['samba_directory_domain_invalid'] = 'Samba Domain téh sah.';
+$lang['samba_directory_initialize_directory'] = 'Initialize Diréktori';
+$lang['samba_directory_password_must_be_7_or_more_characters'] = 'Kecap akses kudu 7 atawa leuwih karakter.';
+$lang['samba_directory_samba_directory_driver_description'] = 'Supir Samba pikeun jasa diréktori';
+$lang['samba_directory_samba_directory_driver'] = 'Samba Diréktori Supir';
+$lang['samba_directory_username_only'] = 'ngaran nu maké Ngan';
+$lang['samba_directory_with_domain'] = 'jeung Domain';
