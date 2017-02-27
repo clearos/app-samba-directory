bd84360df24251efc8b306d2eef166f7faa2e581
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 04:45:56 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lo/samba_directory_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['samba_directory_app_description'] = 'ການ app Samba Directory ສະຫນອງເຄື່ອງມືທີ່ຈໍາເປັນສໍາລັບຜູ້ຊົມໃຊ້, ກຸ່ມ, ການບັນຊີແລະການບໍລິການລະບົບອື່ນໆ.';
+$lang['samba_directory_app_name'] = 'Samba Directory';
+$lang['samba_directory_beta_warning'] = 'ການ Samba Directory app ແມ່ນການທົດລອງ!';
+$lang['samba_directory_domain_invalid'] = 'Samba Domain ບໍ່ຖືກຕ້ອງ.';
+$lang['samba_directory_initialize_directory'] = 'ເລີ່ມຕົ້ນລະບົບ';
+$lang['samba_directory_password_must_be_7_or_more_characters'] = 'ລະຫັດຜ່ານຕ້ອງມີ 7 ຕົວຫລືຫລາຍກວ່າ.';
+$lang['samba_directory_samba_directory_driver_description'] = 'ຄົນຂັບ Samba ສໍາລັບການບໍລິການລະບົບ';
+$lang['samba_directory_samba_directory_driver'] = 'Driver Samba Directory';
+$lang['samba_directory_username_only'] = 'ຊື່ຜູ້ໃຊ້ພຽງແຕ່';
+$lang['samba_directory_with_domain'] = 'ກັບ Domain';
