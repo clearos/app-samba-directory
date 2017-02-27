22f063739565528866459b41084d8e5ffb1b76a0
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 04:39:53 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hy/samba_directory_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['samba_directory_app_description'] = 'The Samba Ուղեցույց ծրագիրը ապահովում է անհրաժեշտ գործիքները օգտվողների համար, խմբերի, հաշիվների եւ այլ ցուցակում:';
+$lang['samba_directory_app_name'] = 'Samba Ուղեցույց';
+$lang['samba_directory_beta_warning'] = 'The Samba Ուղեցույց հավելվածը բետա!';
+$lang['samba_directory_domain_invalid'] = 'Samba Դոմեն է անվավեր:';
+$lang['samba_directory_initialize_directory'] = 'initialize ՏԵՂԵԿԱՏՈՒ';
+$lang['samba_directory_password_must_be_7_or_more_characters'] = 'Գաղտնաբառ պետք է լինի 7 կամ ավելի նիշ.';
+$lang['samba_directory_samba_directory_driver_description'] = 'The Samba վարորդ տեղեկատու ծառայությունների';
+$lang['samba_directory_samba_directory_driver'] = 'Samba Ուղեցույց Վարորդ';
+$lang['samba_directory_username_only'] = 'Մականուն Միայն';
+$lang['samba_directory_with_domain'] = 'With իրավունքը';
